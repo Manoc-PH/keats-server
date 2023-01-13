de1da371d6aeeb7296d9b84494f4407d1a003a23
cloyd <abadcloyd@gmail.com>
2023-01-13 23:33:50 +0800

    updated account_vitals and activity_level models

--- models/account_vitals.go
+++ models/account_vitals.go
@@ -10,8 +10,8 @@ type Account_Vitals struct {
 	ID         uuid.UUID `json:"id"`
 	Account_Id uuid.UUID `json:"account_id"`
 	// info
-	Weight          uint      `json:"weight"`
-	Height          uint      `json:"height"`
+	Weight          int       `json:"weight"`
+	Height          int       `json:"height"`
 	Birthday        time.Time `json:"birthday"`
 	Sex             string    `json:"sex"`
 	Activity_Lvl_Id uuid.UUID `json:"activity_lvl_id"`
--- models/activity_lvl.go
+++ models/activity_lvl.go
@@ -10,5 +10,5 @@ type Activity_Lvl struct {
 	Main_Image_Link   string    `json:"main_image_link"`
 	Background_Color  string    `json:"background_color"`
 	Activity_Lvl_Desc string    `json:"activity_lvl_desc"`
-	Bmr_Multipler     float32   `json:"bmr_multipler"`
+	Bmr_Multiplier    float32   `json:"bmr_multipler"`
 }
