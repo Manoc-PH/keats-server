049616457169529286ddd48503af33fdf3c03777
cloyd <abadcloyd@gmail.com>
2023-01-18 13:23:27 +0800

    fixed bug on get_macros_list now manually tested

--- handlers/tracker/get_macros_list.go
+++ handlers/tracker/get_macros_list.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"math"
+	constants "server/constants/formats"
 	"server/middlewares"
 	"server/models"
 	schemas "server/schemas/tracker"
@@ -27,42 +28,34 @@ func Get_Macros_List(c *fiber.Ctx, db *sql.DB) error {
 		log.Println("Get_Macros_List | Error on query validation: ", err.Error())
 		return c.Status(fiber.StatusBadRequest).JSON(err_data)
 	}
-	days := int(math.Floor(reqData.End_Date.Sub(reqData.Start_Date).Hours() / 24))
-
-	macros := make([]models.Macros, days)
 	// querying macros
-	rows, err := query_macros_list(db, id, reqData)
+	macros_list, err := query_and_scan_macros_list(db, id, reqData)
 	if err != nil && err != sql.ErrNoRows {
-		log.Println("Get_Macros_List | error in querying macros: ", err.Error())
 		return utilities.Send_Error(c, "An error occured", fiber.StatusInternalServerError)
 	}
-	defer rows.Close()
-	// scanning and returning error
-	err = scan_macros_list(rows, macros)
-	// Server Error
-	if err != nil && err != sql.ErrNoRows {
-		log.Println("Get_Macros_List | error in scanning macros: ", err.Error())
-		return utilities.Send_Error(c, "An error occured", fiber.StatusInternalServerError)
-	}
-	return c.Status(fiber.StatusOK).JSON(macros)
+	return c.Status(fiber.StatusOK).JSON(macros_list)
 }
 
-func query_macros_list(db *sql.DB, user_id uuid.UUID, reqData *schemas.Req_Get_Macros_List) (*sql.Rows, error) {
-	row, err := db.Query(`SELECT
+func query_and_scan_macros_list(db *sql.DB, user_id uuid.UUID, reqData *schemas.Req_Get_Macros_List) ([]models.Macros, error) {
+	rows, err := db.Query(`SELECT
 			id, date_created, calories, protein, carbs, fats, 
 			total_calories, total_protein, total_carbs, total_fats,
 			activity_lvl_id, diet_plan_id
 		FROM macros WHERE account_id = $1
-		AND date_created::date BETWEEN $2 AND $3
-		ORDER BY date_Created asc`,
-		// casting timestamp to date
-		user_id, reqData.Start_Date, reqData.End_Date,
+		AND date_created BETWEEN $2 AND $3
+		ORDER BY date_Created desc`,
+		user_id, reqData.Start_Date.Format(constants.YYYY_MM_DD), reqData.End_Date.Format(constants.YYYY_MM_DD),
 	)
-	return row, err
-}
-func scan_macros_list(rows *sql.Rows, macros []models.Macros) error {
+	if err != nil {
+		log.Println("Get_Macros_List | error in querying macros: ", err.Error())
+		return nil, err
+	}
+	defer rows.Close()
+
+	days := int(math.Floor(reqData.End_Date.Sub(reqData.Start_Date).Hours() / 24))
+	macros := make([]models.Macros, 0, days)
 	for rows.Next() {
-		new_macros := models.Macros{}
+		var new_macros = models.Macros{Account_Id: user_id}
 		if err := rows.
 			Scan(
 				&new_macros.ID,
@@ -79,9 +72,10 @@ func scan_macros_list(rows *sql.Rows, macros []models.Macros) error {
 				&new_macros.Activity_Lvl_Id,
 				&new_macros.Diet_Plan_Id,
 			); err != nil {
-			return err
+			log.Println("Get_Macros_List | error in scanning macros: ", err.Error())
+			return nil, err
 		}
 		macros = append(macros, new_macros)
 	}
-	return nil
+	return macros, nil
 }
