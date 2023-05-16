cb8349624aa7155846a1e098df6f8e99a1223224
Cloyd Abad <abadcloyd@gmail.com>
2023-05-16 21:11:04 +0800

    updated delete intake handler

--- handlers/consumer/tracker/delete_intake.go
+++ handlers/consumer/tracker/delete_intake.go
@@ -32,12 +32,12 @@ func Delete_Intake(c *fiber.Ctx, db *sql.DB) error {
 
 	//* data processing
 	intake := models.Intake{}
-	food := models.Food{}
-	food_nutrient := models.Nutrient{}
-	d_nutrients_curr := models.Daily_Nutrients{}
+	nutrient := models.Nutrient{}
+	daily_nutrients := models.Daily_Nutrients{}
 	// TODO OPTIMIZATION: USE GO ROUTINES
+	// Querying intake
 	row := query_intake(db, owner_id, reqData.Intake_ID)
-	// err = scan_intake_food(row, &intake, &food, &food_nutrient)
+	err = scan_intake(row, &intake)
 	if err == sql.ErrNoRows {
 		return utilities.Send_Error(c, "intake not found", fiber.StatusBadRequest)
 	}
@@ -45,80 +45,141 @@ func Delete_Intake(c *fiber.Ctx, db *sql.DB) error {
 		log.Println("Delete_Intake | Error on scanning food: ", err.Error())
 		return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
 	}
+	// Not allowing user to delete past intake
 	is_intake_today := check_if_date_is_today(intake.Date_Created, time.Now())
 	if !is_intake_today {
 		log.Println("Delete_Intake | Error: User trying to delete old intake")
 		return utilities.Send_Error(c, "cannot delete intake from more than a day ago", fiber.StatusBadRequest)
 	}
+	// Querying daily intake
 	row = query_daily_nutrients(db, owner_id)
-	err = scan_daily_nutrients(row, &d_nutrients_curr)
+	err = scan_daily_nutrients(row, &daily_nutrients)
 	if err != nil {
 		log.Println("Delete_Intake | Error on scanning daily_nutrients: ", err.Error())
 		return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
 	}
-	d_nutrients_to_delete := models.Nutrient{ID: d_nutrients_curr.ID}
-	calc_nutrients(&d_nutrients_to_delete, &food_nutrient, intake.Amount)
-	coins, xp, deductions := utilities.Calc_CnXP_On_Intake(float32(d_nutrients_to_delete.Calories), float32(d_nutrients_curr.Calories-d_nutrients_to_delete.Calories), float32(d_nutrients_curr.Max_Calories))
-	coins = coins - deductions
-	xp = xp - deductions
-
-	err = delete_intake_d_nutrients_and_gamestat(db, &d_nutrients_to_delete, coins, xp, &intake)
-	if err != nil {
-		log.Println("Delete_Intake | Error on delete_intake_d_nutrients_and_gamestat: ", err.Error())
-		return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
+	// TODO ADD HANDLER FOR FOOD
+	// Querying nutrient of intake
+	if intake.Ingredient_Mapping_Id != 0 {
+		row = query_ingredient_nutrient(intake.Ingredient_Mapping_Id, db)
+		err = scan_ingredient_nutrient(row, &nutrient)
+		if err != nil {
+			log.Println("Delete_Intake | Error on scanning ingredient nutrients: ", err.Error())
+			return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
+		}
 	}
-	// response_data.Deleted_Coins_And_XP = schemas.Added_Coins_And_XP{Coins: coins * -1, XP: xp * -1}
-	response_data.Deleted_Daily_Nutrients = schemas.Deleted_Daily_Nutrients{
-		Calories: d_nutrients_to_delete.Calories * -1,
-		Protein:  d_nutrients_to_delete.Protein * -1,
-		Carbs:    d_nutrients_to_delete.Carbs * -1,
-		Fats:     d_nutrients_to_delete.Carbs * -1}
-	response_data.Intake = intake
-	response_data.Food = food
-
-	return c.Status(fiber.StatusOK).JSON(response_data)
-}
-
-func delete_intake_d_nutrients_and_gamestat(db *sql.DB, d_nutrients_to_delete *models.Nutrient, coins int, xp int, intake *models.Intake) error {
+	if intake.Food_Id != 0 {
+	}
+	// Calculate nutrients to be deleted
+	nutrients_to_delete := models.Nutrient{}
+	calc_nutrients(&nutrients_to_delete, &nutrient, intake.Amount)
+	// Delete intake and nutrients
 	txn, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = txn.Exec(
-		`UPDATE daily_nutrients SET
-			calories = calories - $1,
-			protein = protein - $2,
-			carbs = carbs - $3,
-			fats = fats - $4
-		WHERE id = $5`,
-		d_nutrients_to_delete.Calories,
-		d_nutrients_to_delete.Protein,
-		d_nutrients_to_delete.Carbs,
-		d_nutrients_to_delete.Fats,
-		d_nutrients_to_delete.ID,
-	)
+	err = delete_intake(txn, &intake)
 	if err != nil {
-		log.Println("delete_intake_d_nutrients_and_gamestat (update daily_nutrients) | Error: ", err.Error())
-		return err
+		log.Println("Delete_Intake | Error on delete_intake: ", err.Error())
+		return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
 	}
-	_, err = txn.Exec(
-		`UPDATE account_game_stat SET coins = coins - $1, xp = xp - $2 WHERE account_id = $3`,
-		coins, xp, intake.Account_Id,
-	)
+	daily_nutrients_to_delete := models.Daily_Nutrients{ID: daily_nutrients.ID}
+	scan_and_inverse_daily_nutrients(&daily_nutrients_to_delete, &nutrients_to_delete)
+	err = update_daily_nutrients(txn, &daily_nutrients_to_delete)
 	if err != nil {
-		log.Println("delete_intake_d_nutrients_and_gamestat (update account_game_stat)| Error: ", err.Error())
-		return err
+		log.Println("Delete_Intake | Error on delete_intake: ", err.Error())
+		return utilities.Send_Error(c, err.Error(), fiber.StatusInternalServerError)
 	}
-	_, err = txn.Exec(`DELETE FROM intake WHERE id = $1`, intake.ID)
-	if err != nil {
-		log.Println("delete_intake_d_nutrients_and_gamestat (update intake)| Error: ", err.Error())
-		return err
-	}
+
 	err = txn.Commit()
 	if err != nil {
 		txn.Rollback()
-		log.Println("delete_intake_d_nutrients_and_gamestat (commit) | Error: ", err.Error())
+		log.Println("Delete_Intake (commit) | Error: ", err.Error())
+		return err
+	}
+	// response_data.Deleted_Coins_And_XP = schemas.Added_Coins_And_XP{Coins: coins * -1, XP: xp * -1}
+	inverse_nutrient(&nutrients_to_delete)
+	response_data.Deleted_Daily_Nutrients = nutrients_to_delete
+	response_data.Intake = intake
+
+	return c.Status(fiber.StatusOK).JSON(response_data)
+}
+
+func delete_intake(txn *sql.Tx, intake *models.Intake) error {
+	_, err := txn.Exec(`DELETE FROM intake WHERE id = $1`, intake.ID)
+	if err != nil {
+		log.Println("Delete_Intake (delete_intake) | Error: ", err.Error())
 		return err
 	}
 	return nil
 }
+func scan_and_inverse_daily_nutrients(daily_nutrients *models.Daily_Nutrients, nutrients_to_delete *models.Nutrient) {
+	daily_nutrients.Calories = nutrients_to_delete.Calories * -1
+	daily_nutrients.Protein = nutrients_to_delete.Protein * -1
+	daily_nutrients.Carbs = nutrients_to_delete.Carbs * -1
+	daily_nutrients.Fats = nutrients_to_delete.Fats * -1
+	daily_nutrients.Trans_Fat = nutrients_to_delete.Trans_Fat * -1
+	daily_nutrients.Saturated_Fat = nutrients_to_delete.Saturated_Fat * -1
+	daily_nutrients.Sugars = nutrients_to_delete.Sugars * -1
+	daily_nutrients.Fiber = nutrients_to_delete.Fiber * -1
+	daily_nutrients.Sodium = nutrients_to_delete.Sodium * -1
+	daily_nutrients.Iron = nutrients_to_delete.Iron * -1
+	daily_nutrients.Calcium = nutrients_to_delete.Calcium * -1
+}
+func inverse_nutrient(nutrient *models.Nutrient) {
+	nutrient.Calories = nutrient.Calories * -1
+	nutrient.Protein = nutrient.Protein * -1
+	nutrient.Carbs = nutrient.Carbs * -1
+	nutrient.Fats = nutrient.Fats * -1
+	nutrient.Trans_Fat = nutrient.Trans_Fat * -1
+	nutrient.Saturated_Fat = nutrient.Saturated_Fat * -1
+	nutrient.Sugars = nutrient.Sugars * -1
+	nutrient.Fiber = nutrient.Fiber * -1
+	nutrient.Sodium = nutrient.Sodium * -1
+	nutrient.Iron = nutrient.Iron * -1
+	nutrient.Calcium = nutrient.Calcium * -1
+}
+
+// func delete_intake_d_nutrients_and_gamestat(db *sql.DB, d_nutrients_to_delete *models.Nutrient, coins int, xp int, intake *models.Intake) error {
+// 	txn, err := db.Begin()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	_, err = txn.Exec(
+// 		`UPDATE daily_nutrients SET
+// 			calories = calories - $1,
+// 			protein = protein - $2,
+// 			carbs = carbs - $3,
+// 			fats = fats - $4
+// 		WHERE id = $5`,
+// 		d_nutrients_to_delete.Calories,
+// 		d_nutrients_to_delete.Protein,
+// 		d_nutrients_to_delete.Carbs,
+// 		d_nutrients_to_delete.Fats,
+// 		d_nutrients_to_delete.ID,
+// 	)
+// 	if err != nil {
+// 		log.Println("delete_intake_d_nutrients_and_gamestat (update daily_nutrients) | Error: ", err.Error())
+// 		return err
+// 	}
+// 	_, err = txn.Exec(
+// 		`UPDATE account_game_stat SET coins = coins - $1, xp = xp - $2 WHERE account_id = $3`,
+// 		coins, xp, intake.Account_Id,
+// 	)
+// 	if err != nil {
+// 		log.Println("delete_intake_d_nutrients_and_gamestat (update account_game_stat)| Error: ", err.Error())
+// 		return err
+// 	}
+// 	_, err = txn.Exec(`DELETE FROM intake WHERE id = $1`, intake.ID)
+// 	if err != nil {
+// 		log.Println("delete_intake_d_nutrients_and_gamestat (update intake)| Error: ", err.Error())
+// 		return err
+// 	}
+// 	err = txn.Commit()
+// 	if err != nil {
+// 		txn.Rollback()
+// 		log.Println("delete_intake_d_nutrients_and_gamestat (commit) | Error: ", err.Error())
+// 		return err
+// 	}
+// 	return nil
+// }
--- schemas/tracker/delete_intake.go
+++ schemas/tracker/delete_intake.go
@@ -7,19 +7,7 @@ type Req_Delete_Intake struct {
 	Intake_ID uint `json:"intake_id" validate:"required"`
 }
 
-type Deleted_Daily_Nutrients struct {
-	Calories      float32 `json:"calories"`
-	Protein       float32 `json:"protein"`
-	Carbs         float32 `json:"carbs"`
-	Fats          float32 `json:"fats"`
-	Trans_Fat     float32 `json:"trans_fat"`
-	Saturated_Fat float32 `json:"saturated_fat"`
-	Sugars        float32 `json:"sugars"`
-	Sodium        float32 `json:"sodium"`
-}
 type Res_Delete_Intake struct {
-	Deleted_Daily_Nutrients Deleted_Daily_Nutrients `json:"deleted_macros"`
-	// Deleted_Coins_And_XP    Added_Coins_And_XP      `json:"deleted_coins_and_xp"`
-	Intake models.Intake `json:"intake"`
-	Food   models.Food   `json:"food"`
+	Deleted_Daily_Nutrients models.Nutrient `json:"deleted_daily_nutrients"`
+	Intake                  models.Intake   `json:"intake"`
 }
