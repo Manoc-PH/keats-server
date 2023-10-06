5268e4e6b2b3838cc723c478ffc6505f28e5b976
Cloyd Abad <abadcloyd@gmail.com>
2023-10-06 18:46:50 +0800

    changed what get daily nutrients returned

--- handlers/consumer/tracker/get_daily_nutrients_list.go
+++ handlers/consumer/tracker/get_daily_nutrients_list.go
@@ -6,7 +6,6 @@ import (
 	"math"
 	constants "server/constants/formats"
 	"server/middlewares"
-	"server/models"
 	schemas "server/schemas/consumer/tracker"
 	"server/utilities"
 
@@ -37,15 +36,12 @@ func Get_Daily_Nutrients_List(c *fiber.Ctx, db *sql.DB) error {
 	return c.Status(fiber.StatusOK).JSON(d_nutrients_list)
 }
 
-func query_and_scan_d_nutrients_list(db *sql.DB, user_id uuid.UUID, reqData *schemas.Req_Get_Daily_Nutrients_List) ([]models.Daily_Nutrients, error) {
+func query_and_scan_d_nutrients_list(db *sql.DB, user_id uuid.UUID, reqData *schemas.Req_Get_Daily_Nutrients_List) ([]schemas.Res_Get_Daily_Nutrients_List, error) {
 	rows, err := db.Query(`SELECT
-			id, date_created, calories, protein, carbs, fats, 
-			max_calories, max_protein, max_carbs, max_fats,
-			activity_lvl_id, diet_plan_id,
-			trans_fat, saturated_fat, sugars, fiber, sodium, iron, calcium
+			id, date_created, calories
 		FROM daily_nutrients WHERE account_id = $1
 		AND date_created BETWEEN $2 AND $3
-		ORDER BY date_Created desc`,
+		ORDER BY date_created desc`,
 		user_id, reqData.Start_Date.Format(constants.YYYY_MM_DD), reqData.End_Date.Format(constants.YYYY_MM_DD),
 	)
 	if err != nil {
@@ -55,32 +51,14 @@ func query_and_scan_d_nutrients_list(db *sql.DB, user_id uuid.UUID, reqData *sch
 	defer rows.Close()
 
 	days := int(math.Floor(reqData.End_Date.Sub(reqData.Start_Date).Hours() / 24))
-	daily_nutrients := make([]models.Daily_Nutrients, 0, days)
+	daily_nutrients := make([]schemas.Res_Get_Daily_Nutrients_List, 0, days)
 	for rows.Next() {
-		var new_daily_nutrient = models.Daily_Nutrients{Account_Id: user_id}
+		var new_daily_nutrient = schemas.Res_Get_Daily_Nutrients_List{Account_Id: user_id}
 		if err := rows.
 			Scan(
 				&new_daily_nutrient.ID,
 				&new_daily_nutrient.Date_Created,
 				&new_daily_nutrient.Calories,
-				&new_daily_nutrient.Protein,
-				&new_daily_nutrient.Carbs,
-				&new_daily_nutrient.Fats,
-
-				&new_daily_nutrient.Max_Calories,
-				&new_daily_nutrient.Max_Protein,
-				&new_daily_nutrient.Max_Carbs,
-				&new_daily_nutrient.Max_Fats,
-				&new_daily_nutrient.Activity_Lvl_Id,
-				&new_daily_nutrient.Diet_Plan_Id,
-
-				&new_daily_nutrient.Trans_Fat,
-				&new_daily_nutrient.Saturated_Fat,
-				&new_daily_nutrient.Sugars,
-				&new_daily_nutrient.Fiber,
-				&new_daily_nutrient.Sodium,
-				&new_daily_nutrient.Iron,
-				&new_daily_nutrient.Calcium,
 			); err != nil {
 			log.Println("Get_Daily_Nutrients_List | error in scanning Daily_Nutrients: ", err.Error())
 			return nil, err
