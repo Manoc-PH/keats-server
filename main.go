229fe055f2632596a34ed334995e67f64d76dc63
Cloyd Abad <abadcloyd@gmail.com>
2023-10-07 11:07:10 +0800

    added Comments on main.go

--- main.go
+++ main.go
@@ -33,3 +33,66 @@ func main() {
 
 	log.Fatal(app.Listen(":" + port))
 }
+
+// TODO IMPLEMENT DB CON POOLING
+// SETUP
+// func NewDBPool(connectionString string, maxConnections int) (*sql.DB, error) {
+// 	connStr := connectionString
+
+// 	// Configure the connection pool settings
+// 	config, err := pq.ParseURL(connStr)
+// 	if err != nil {
+// 			return nil, err
+// 	}
+// 	config += fmt.Sprintf(" pool_max_conns=%d", maxConnections)
+
+// 	// Open a new database connection pool
+// 	db, err := sql.Open("postgres", config)
+// 	if err != nil {
+// 			return nil, err
+// 	}
+
+// 	// Test the database connection
+// 	err = db.Ping()
+// 	if err != nil {
+// 			db.Close()
+// 			return nil, err
+// 	}
+
+// 	return db, nil
+// }
+
+// MAIN
+// const (
+// 	dbConnectionString = "your_db_connection_string_here"
+// 	maxDBConnections   = 10 // Set your desired maximum number of connections
+// )
+
+// func main() {
+// 	db, err := NewDBPool(dbConnectionString, maxDBConnections)
+// 	if err != nil {
+// 			log.Fatalf("Failed to connect to the database: %v", err)
+// 	}
+// 	defer db.Close()
+
+// 	// Use the 'db' connection pool in your application's handlers
+// }
+
+// HANDLER
+// func SomeHandler(c *fiber.Ctx) error {
+// 	// Get a connection from the pool
+// 	conn, err := db.BeginTx(context.Background(), nil)
+// 	if err != nil {
+// 			return err
+// 	}
+// 	defer conn.Rollback()
+
+// 	// Execute queries using 'conn'
+
+// 	// Commit the transaction (if successful)
+// 	if err := conn.Commit(); err != nil {
+// 			return err
+// 	}
+
+// 	return nil
+// }
