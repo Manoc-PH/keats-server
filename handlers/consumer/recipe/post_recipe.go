af23091235f1a69dc5f97d75d63fd35f115bd037
Cloyd Abad <abadcloyd@gmail.com>
2023-10-16 15:15:54 +0800

    added post recipe handler (tested)

---
+++ handlers/consumer/recipe/post_recipe.go
@@ -0,0 +1,222 @@
+package handlers
+
+import (
+	"database/sql"
+	"log"
+	"server/middlewares"
+	schemas "server/schemas/consumer/recipe"
+	"server/utilities"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+func Post_Recipe(c *fiber.Ctx, db *sql.DB) error {
+	// auth validation
+	_, owner_id, err := middlewares.AuthMiddleware(c)
+	if err != nil {
+		log.Println("Post_Recipe | Error on auth middleware: ", err.Error())
+		return utilities.Send_Error(c, err.Error(), fiber.StatusUnauthorized)
+	}
+
+	//* data validation
+	reqData := new(schemas.Req_Post_Recipe)
+	if err_data, err := middlewares.Body_Validation(reqData, c); err != nil {
+		log.Println("Post_Recipe | Error on query validation: ", err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(err_data)
+	}
+
+	// Validating ingredients
+	for _, item := range reqData.Recipe_Ingredients {
+		if item.Ingredient_Mapping_Id != 0 {
+			exists := ingredient_exists(item.Ingredient_Mapping_Id, db)
+			if exists == false {
+				log.Println("Post_Recipe | Error: user sent an ingredient that doesn't exist. Ingredient_Mapping_Id: ", item.Ingredient_Mapping_Id)
+				return utilities.Send_Error(c, "An ingredient does not exist.", fiber.StatusBadRequest)
+			}
+		}
+		if item.Food_Id != 0 {
+			exists := food_exists(item.Food_Id, db)
+			if exists == false {
+				log.Println("Post_Recipe | Error: user sent an ingredient that doesn't exist. Food_Id: ", item.Food_Id)
+				return utilities.Send_Error(c, "An ingredient does not exist.", fiber.StatusBadRequest)
+			}
+		}
+	}
+
+	// Saving Recipe
+	err = save_recipe_txn(reqData, db, owner_id)
+	if err != nil {
+		return utilities.Send_Error(c, "An error occured in saving recipe", fiber.StatusInternalServerError)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(reqData)
+}
+func ingredient_exists(ingredient_mapping_id uint, db *sql.DB) bool {
+	row := db.QueryRow(`SELECT id FROM ingredient_mapping WHERE id = $1`, ingredient_mapping_id)
+	found_id := 0
+	err := row.Scan(&found_id)
+	if err != nil {
+		return false
+	}
+	if found_id != 0 {
+		return true
+	}
+	return false
+}
+func food_exists(food_id uint, db *sql.DB) bool {
+	row := db.QueryRow(`SELECT id FROM food WHERE id = $1`, food_id)
+	found_id := 0
+	err := row.Scan(&found_id)
+	if err != nil {
+		return false
+	}
+	if found_id != 0 {
+		return true
+	}
+	return false
+}
+func save_recipe_txn(recipe *schemas.Req_Post_Recipe, db *sql.DB, owner_id uuid.UUID) error {
+	txn, err := db.Begin()
+	if err != nil {
+		log.Println("Post_Recipe | Error on save_recipe_txn: ", err.Error())
+		return err
+	}
+	// Saving Recipe
+	err = save_recipe_details(txn, recipe, owner_id)
+	if err != nil {
+		log.Println("Post_Recipe | Error on save_recipe_details: ", err.Error())
+		return err
+	}
+	// Saving Recipe Ingredients
+	err = save_recipe_ingredients(txn, recipe)
+	if err != nil {
+		log.Println("Post_Recipe | Error on save_recipe_ingredients: ", err.Error())
+		return err
+	}
+	// Saving Recipe Instructions
+	err = save_recipe_instructions(txn, recipe)
+	if err != nil {
+		log.Println("Post_Recipe | Error on save_recipe_instructions: ", err.Error())
+		return err
+	}
+
+	err = txn.Commit()
+	if err != nil {
+		txn.Rollback()
+		log.Println(" (commit) | Error: ", err.Error())
+		return err
+	}
+	return nil
+}
+func save_recipe_details(txn *sql.Tx, recipe *schemas.Req_Post_Recipe, owner_id uuid.UUID) error {
+	// Saving Recipe Details
+	// TODO SAVE CATEGORY ID
+	row := txn.QueryRow(`
+	INSERT INTO recipe(
+		name,
+		name_ph,
+		name_owner,
+		owner_id,
+		date_created,
+		-- category_id,
+		thumbnail_image_link,
+		main_image_link,
+		likes,
+		rating,
+		servings,
+		servings_size,
+		prep_time,
+		description)
+	VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+	RETURNING id
+`,
+		recipe.Recipe.Name,
+		recipe.Recipe.Name_Ph,
+		recipe.Recipe.Name_Owner,
+		owner_id,
+		recipe.Recipe.Date_Created,
+		// recipe.Recipe.Category_Id,
+		recipe.Recipe.Thumbnail_Image_Link,
+		recipe.Recipe.Main_Image_Link,
+		recipe.Recipe.Likes,
+		recipe.Recipe.Rating,
+		recipe.Recipe.Servings,
+		recipe.Recipe.Servings_Size,
+		recipe.Recipe.Prep_Time,
+		recipe.Recipe.Description)
+	err := row.Scan(&recipe.Recipe.ID)
+	if err != nil {
+		log.Println("Post_Recipe | Error on save_recipe_details: ", err.Error())
+		return err
+	}
+	return nil
+}
+func save_recipe_ingredients(txn *sql.Tx, recipe *schemas.Req_Post_Recipe) error {
+	// Saving Recipe Ingredients
+	stmt, err := txn.Prepare(
+		`INSERT INTO recipe_ingredient (
+				food_id,
+				ingredient_mapping_id,
+				amount,
+				amount_unit,
+				amount_unit_desc,
+				serving_size)
+			VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+	)
+	if err != nil {
+		log.Println("save_recipe_ingredient (Prepare) | Error: ", err.Error())
+		return err
+	}
+	defer stmt.Close()
+	// Insert each row
+	for i, item := range recipe.Recipe_Ingredients {
+		row := stmt.QueryRow(
+			item.Food_Id,
+			item.Ingredient_Mapping_Id,
+			item.Amount,
+			item.Amount_Unit,
+			item.Amount_Unit_Desc,
+			item.Serving_Size,
+		)
+		id := 0
+		err = row.Scan(&id)
+		recipe.Recipe_Ingredients[i].ID = uint(id)
+		if err != nil {
+			log.Println("save_recipe_ingredient (Exec) | Error: ", err.Error())
+			return err
+		}
+	}
+	return nil
+}
+func save_recipe_instructions(txn *sql.Tx, recipe *schemas.Req_Post_Recipe) error {
+	// Saving Recipe Instructions
+	stmt, err := txn.Prepare(
+		`INSERT INTO recipe_instruction (
+				recipe_id,
+				instruction_description,
+				step_num)
+			VALUES ($1, $2, $3) RETURNING id`,
+	)
+	if err != nil {
+		log.Println("save_recipe_ingredient (Prepare) | Error: ", err.Error())
+		return err
+	}
+	defer stmt.Close()
+	// Insert each row
+	for i, item := range recipe.Recipe_Instructions {
+		row := stmt.QueryRow(
+			recipe.Recipe.ID,
+			item.Instruction_Description,
+			item.Step_Num,
+		)
+		id := 0
+		err = row.Scan(&id)
+		recipe.Recipe_Instructions[i].ID = uint(id)
+		if err != nil {
+			log.Println("save_recipe_ingredient (Exec) | Error: ", err.Error())
+			return err
+		}
+	}
+	return nil
+}
---
+++ schemas/consumer/recipe/post_recipe.go
@@ -0,0 +1,48 @@
+package schemas
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// *REQUESTS
+type Req_Post_Recipe struct {
+	Recipe              Recipe_Schema               `json:"recipe"`
+	Recipe_Ingredients  []Recipe_Ingredient_Schema  `json:"recipe_ingredients" validate:"required"`
+	Recipe_Instructions []Recipe_Instruction_Schema `json:"recipe_instructions" validate:"required"`
+}
+
+// Schemas
+type Recipe_Schema struct {
+	ID                   uint      `json:"id"`
+	Name                 string    `json:"name" validate:"required"`
+	Name_Ph              string    `json:"name_ph"`
+	Name_Owner           string    `json:"name_owner" validate:"required"`
+	Owner_Id             uuid.UUID `json:"owner_id" validate:"required"`
+	Date_Created         time.Time `json:"date_created"`
+	Category_Id          uint      `json:"category_id"`
+	Thumbnail_Image_Link string    `json:"thumbnail_image_link"`
+	Main_Image_Link      string    `json:"main_image_link"`
+	Likes                uint      `json:"likes"`
+	Rating               uint      `json:"rating"`
+	Servings             uint      `json:"servings" validate:"required"`
+	Servings_Size        uint      `json:"servings_size" validate:"required"`
+	Prep_Time            uint      `json:"prep_time" validate:"required"`
+	Description          string    `json:"description"`
+}
+type Recipe_Ingredient_Schema struct {
+	ID                    uint    `json:"id"`
+	Food_Id               uint    `json:"food_id" validate:"required_if=Ingredient_Mapping_Id 0"`
+	Ingredient_Mapping_Id uint    `json:"ingredient_mapping_id" validate:"required_if=Food_Id 0"`
+	Amount                float32 `json:"amount" validate:"required"`
+	Amount_Unit           string  `json:"amount_unit"`
+	Amount_Unit_Desc      string  `json:"amount_unit_desc"`
+	Serving_Size          float32 `json:"serving_size"`
+}
+type Recipe_Instruction_Schema struct {
+	ID                      uint   `json:"id"`
+	Recipe_Id               uint   `json:"recipe_id"`
+	Instruction_Description string `json:"instruction_description" validate:"required"`
+	Step_Num                uint   `json:"step_num"`
+}
