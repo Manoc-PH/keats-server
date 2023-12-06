25b3974cdea639007eecf4bb637a691949fc71c6
Cloyd Abad <abadcloyd@gmail.com>
2023-12-06 12:33:40 +0800

    updated get recipe filtered schema

--- schemas/consumer/recipe/get_recipe_filtered.go
+++ schemas/consumer/recipe/get_recipe_filtered.go
@@ -3,9 +3,9 @@ package schemas
 import "server/models"
 
 type Req_Get_Recipe_Filtered struct {
-	Filter  string `json:"filter" validate:"required,oneof=h_protein h_carbs h_fats l_cal l_carbs l_fats"`
-	Created bool   `json:"created"`
-	Liked   bool   `json:"liked"`
+	Filter  string `json:"filter" validate:"required_if_empty=Created Liked,oneof=h_protein h_carbs h_fats l_cal l_carbs l_fats"`
+	Created bool   `json:"created" validate:"required_if_empty=Filter Liked"`
+	Liked   bool   `json:"liked" validate:"required_if_empty=Filter Created"`
 }
 
 type Res_Get_Recipe_Filtered struct {
